b9f19453efacee135ca7b29c9f95df4077feee7a
Arthur-FSchmidt <arthurfschmidt1@gmail.com>
2025-04-29 16:09:21 -0300

    Adiciona a tela de erro 404

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Rotas
 app.get('/', (req, res) => {
-  res.render('index', {title:"Informática"})
+  res.render('index', {title:"Informática"});
 });
 
 app.use((req, res, next) => {
-    res.status(404).json({ error: "Not Found" });
+    res.render('404', {title:"Informática"});
   });
 
 // Start Server
