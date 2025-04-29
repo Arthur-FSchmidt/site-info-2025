7f9fa7f2e45a9f708e0fc5f7d903f9add7b6318f
Arthur-FSchmidt <arthurfschmidt1@gmail.com>
2025-04-29 11:29:31 -0300

    Começo do site com Express e Pug, Criação das view

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+require('dotenv').config();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Adicionar css e bootstrap
+app.use(express.static('public'));
+const path = require('path');
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Not Found" });
+  });
+
+// Start Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
