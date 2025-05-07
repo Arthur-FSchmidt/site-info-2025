69f5fa1cf1615bd0403a3934fcc8566f2de4af75
Arthur-FSchmidt <arthurfschmidt1@gmail.com>
2025-05-07 09:09:27 -0300

    Cria a página Feiras

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.get('/curriculo', (req, res) => {
 });
 
 app.get('/feiras', (req, res) => {
-  res.render('feiras', {title:"Feiras - Informática"});
+  let feiras = [{nome:"Feira a - 2025", imagem_path:"media/image.png", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec mattis lacus. Curabitur eget est mattis, imperdiet risus sit amet, luctus leo. Suspendisse ullamcorper tempus mauris, at malesuada dolor hendrerit vel. Phasellus sed sagittis sapien. Sed lacinia faucibus rhoncus."}, {nome:"Feira a - 2025", imagem_path:"media/image.png", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec mattis lacus. Curabitur eget est mattis, imperdiet risus sit amet, luctus leo. Suspendisse ullamcorper tempus mauris, at malesuada dolor hendrerit vel. Phasellus sed sagittis sapien. Sed lacinia faucibus rhoncus."}, {nome:"Feira a - 2025", imagem_path:"media/image.png", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec mattis lacus. Curabitur eget est mattis, imperdiet risus sit amet, luctus leo. Suspendisse ullamcorper tempus mauris, at malesuada dolor hendrerit vel. Phasellus sed sagittis sapien. Sed lacinia faucibus rhoncus."}]
+  res.render('feiras', {title:"Feiras - Informática", feiras:feiras});
 });
 
 app.get('/professores', (req, res) => {
